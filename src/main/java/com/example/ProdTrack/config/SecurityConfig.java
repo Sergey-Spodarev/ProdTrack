15b80df1806f652d8333d05e02069c2b3cb28c03
Sergey-Spodarev <spodarev8@gmail.com>
2025-07-18 17:30:49 +0300

    Everything is working

--- src/main/java/com/example/ProdTrack/config/SecurityConfig.java
+++ src/main/java/com/example/ProdTrack/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.ProdTrack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,11 +31,23 @@ public class SecurityConfig {
                         authorizeRequests.requestMatchers("/", "/login", "/api/user/register/**", "/api/admin/**", "/api/report/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(formLogin ->formLogin
-                        .loginPage("/")// страница с формой
-                        .loginProcessingUrl("/login") // URL для обработки формы
-                        .defaultSuccessUrl("/")// куда перенаправлять пользователя после успешного входа
-                        .permitAll()
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .successHandler((request, response, authentication) -> {
+                            String redirectUrl = "/"; // дефолтный путь
+
+                            if (authentication.getAuthorities().stream()
+                                    .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ADMIN"))) {
+                                redirectUrl = "/admin";
+                            } else if (authentication.getAuthorities().stream()
+                                    .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("USER"))) {
+                                redirectUrl = "/user";
+                            }
+
+                            response.sendRedirect(redirectUrl);
+                        })
+                        .failureHandler((request, response, ex) -> response.setStatus(HttpStatus.UNAUTHORIZED.value()))
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
